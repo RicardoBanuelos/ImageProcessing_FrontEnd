20ad796e1de0fdb9505c6e3901b7cbefbb5b7c8a
Ricardo Bañuelos De La Torre <47264662+RicardoBanuelos@users.noreply.github.com>
2024-05-24 10:01:18 -0700

    Changed from create-react-app to vite

--- image_processing/src/App.tsx
+++ image_processing/src/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import './App.css';
+import Home from './pages/Home/Home'
 
 function App() {
   return (
-    <div>
-      
-    </div>
-  );
+    <Home/>
+  )
 }
 
-export default App;
+export default App
---
+++ image_processing/src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="title_div">
+            <p className="title_p">
+                Image Processing
+            </p>
+        </div>
+    )
+}
+
+export default Home
